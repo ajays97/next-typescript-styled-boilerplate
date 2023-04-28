da77b34f7d110d48e7c03a00a65a4168b86fb81f
ajays97 <ajaysrinivas530@gmail.com>
2023-04-28 20:16:28 +0530

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+const defaultColor = '#565660';
+
 export const Header = styled.header`
 	position: relative;
 	top: 0;
@@ -7,7 +9,9 @@ export const Header = styled.header`
 	width: 100%;
 	z-index: 9999;
 	height: 50px;
-	background: black;
+	background: ${defaultColor};
 	color: white;
-	@include flex(center, center);
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
